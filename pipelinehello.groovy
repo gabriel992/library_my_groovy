b0fb231338e62462643ad6b6aee18efa79dbf207
gabriel992 <awsgabriel992@gmail.com>
2021-08-06 22:19:19 -0300

    config  pipeline

---
+++ pipelinehello.groovy
@@ -0,0 +1,29 @@
+import java.util.concurrent.TimeUnit
+import groovy.json.*
+
+def call(map){
+    pipeline {
+        agent any
+
+        stages {
+            stage('Build') {
+                steps {
+                    echo 'Building..'
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Testing..'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo 'Deploying....'
+                }
+            }
+        }
+        
+    
+
+    } 
+}
